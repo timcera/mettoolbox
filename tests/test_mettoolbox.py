d20ff69472971846c8966f63677d6519c47ad349
Tim Cera <tim@cerazone.net>
2020-04-02 09:18:34 -0400

    test: Tests are all in tests/
    The tests in melodist no longer work but were being found by pytest.
    Now "detox" and ".travis.yml" explicitly point to the top level tests/
    directory.
    
    Added first test to top level tests/ directory.

--- tests/test_mettoolbox.py
+++ tests/test_mettoolbox.py
@@ -10,15 +10,19 @@ Tests for `mettoolbox` module.
 
 import unittest
 
+from pandas.testing import assert_frame_equal
 from mettoolbox import mettoolbox
+from tstoolbox import tstoolbox
 
 
 class TestMettoolbox(unittest.TestCase):
     def setUp(self):
-        pass
+        self.disaggregate_temperature = tstoolbox.read("tests/data_temperature_gainesville_disaggregate_sine_mean.csv")
 
-    def test_something(self):
-        pass
+    def test_disaggregate_temperature(self):
+        out = mettoolbox.disaggregate.temperature("sine_mean", ["degC", "degC"], temp_min_col=1, temp_max_col=2, input_ts="tests/data_temperature_gainesville.csv")
+        out.index.name = "Datetime"
+        assert_frame_equal(out, self.disaggregate_temperature)
 
     def tearDown(self):
         pass
